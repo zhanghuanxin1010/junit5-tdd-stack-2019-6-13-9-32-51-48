bbd6e1238158e0c70c3770ceafc7885c53710e2b
unknown <1012151538@qq.comgit config --global user.name >
2019-08-23 18:25:09 +0800

    ceeshi

--- src/main/java/com/thoughtworks/tdd/HelloWorld.java
+++ src/main/java/com/thoughtworks/tdd/HelloWorld.java
@@ -3,14 +3,50 @@ package com.thoughtworks.tdd;
 /**
  * Created by twer on 3/28/14.
  */
+//public class HelloWorld {
+//    private Dependency dependency;
+//
+//    public HelloWorld(Dependency dependency) {
+//
+//        this.dependency = dependency;
+//    }
+//    public String beenCalled() {
+//        return dependency.say();
+//    }
+//}
 public class HelloWorld {
-    private Dependency dependency;
+	public HelloWorld() {}
 
-    public HelloWorld(Dependency dependency) {
-
-        this.dependency = dependency;
-    }
-    public String beenCalled() {
-        return dependency.say();
-    }
+	@SuppressWarnings("unused")
+	public String fizzBuzz(int i) {
+		String result="";
+		String Fizz="Fizz";
+		String Whizz="Whizz";
+		String Buzz="Buzz";
+		if(i<1||i>120)
+		{
+			return "valid";
+		}
+			
+		if(i%7==0) {
+			result+=Whizz;
+		}
+		if(i%5==0)
+		{
+			result+=Buzz;
+		}
+		if(i%3==0)
+		{
+			result+=Fizz;
+		}
+		if(result=="")
+			
+			{return String.valueOf(i);}
+		else
+		
+		{return result;}
+		
+	}
+	
+	
 }
--- src/test/java/com/thoughtworks/tdd/HelloWorldTest.java
+++ src/test/java/com/thoughtworks/tdd/HelloWorldTest.java
@@ -1,16 +1,14 @@
 package com.thoughtworks.tdd;
 
-import com.thoughtworks.tdd.Dependency;
-import com.thoughtworks.tdd.HelloWorld;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import com.thoughtworks.tdd.Dependency;
+import com.thoughtworks.tdd.HelloWorld;
+
+
 
 
 public class HelloWorldTest {
@@ -23,7 +21,7 @@ public class HelloWorldTest {
     void initEveryMethod() {
     }
 
-
+/* 
     @Test
     @DisplayName("try display name")
     public void hello_world_test() {
@@ -50,5 +48,65 @@ public class HelloWorldTest {
 
         //then
         assertThat(actual, is("Hello World"));
+    }*/
+    
+    @Test
+    public void should_be_return_number_when_input_i() {
+    	HelloWorld helloWorld=new HelloWorld();
+    	String result= helloWorld.fizzBuzz(1);
+    	Assertions.assertEquals( "1",result);
+        
+        
     }
+    @Test
+    public void should_be_return_Fizz_when_input_3() {
+    	HelloWorld helloWorld=new HelloWorld();
+    	String result= helloWorld.fizzBuzz(3);
+    	Assertions.assertEquals( "Fizz",result);
+        
+        
+    }
+    @Test
+    public void should_be_return_Buzz_when_input_5() {
+    	HelloWorld helloWorld=new HelloWorld();
+    	String result= helloWorld.fizzBuzz(5);
+    	Assertions.assertEquals( "Buzz",result);
+        
+        
+    }
+    @Test
+    public void should_be_return_Whizz_when_input_7() {
+    	HelloWorld helloWorld=new HelloWorld();
+    	String result= helloWorld.fizzBuzz(7);
+    	Assertions.assertEquals( "Whizz",result);
+    }
+    	@Test
+        public void should_be_return_Whizz_when_input_15() {
+        	HelloWorld helloWorld=new HelloWorld();
+        	String result= helloWorld.fizzBuzz(15);
+        	Assertions.assertEquals( "BuzzFizz",result);
+        
+    	}
+    	@Test
+        public void should_be_return__WhizzFizz_when_input_21() {
+        	HelloWorld helloWorld=new HelloWorld();
+        	String result= helloWorld.fizzBuzz(21);
+        	Assertions.assertEquals( "WhizzFizz",result);
+        
+    	}
+    	@Test
+        public void should_be_return__WhizzBuzz_when_input_35() {
+        	HelloWorld helloWorld=new HelloWorld();
+        	String result= helloWorld.fizzBuzz(35);
+        	Assertions.assertEquals( "WhizzBuzz",result);
+        
+    	}
+    	@Test
+        public void should_be_return__WhizzBuzzFizz_when_input_105() {
+        	HelloWorld helloWorld=new HelloWorld();
+        	String result= helloWorld.fizzBuzz(105);
+        	Assertions.assertEquals( "WhizzBuzzFizz",result);
+        
+    	}
+  
 }
